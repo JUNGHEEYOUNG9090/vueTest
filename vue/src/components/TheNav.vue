8e33434da3b2f0dedbf2b5db06d3e4792c27f0e6
sunrise <sunrise@DESKTOP-RA92TKA>
2024-02-08 13:14:05 +0900

    Nav 입힘

---
+++ vue/src/components/TheNav.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-navigation-drawer class="bg-deep-purple" theme="dark" permanent>
+      <v-list color="transparent">
+        <v-list v-model:opened="open">
+          <v-list-item prepend-icon="mdi-home" title="Home"></v-list-item>
+
+          <v-list-group value="Users">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" prepend-icon="mdi-account-circle" title="Users"></v-list-item>
+            </template>
+
+            <v-list-group value="Admin">
+              <template v-slot:activator="{ props }">
+                <v-list-item v-bind="props" title="Admin"></v-list-item>
+              </template>
+
+              <v-list-item v-for="([title, icon], i) in admins" :key="i" :title="title" :prepend-icon="icon"
+                :value="title"></v-list-item>
+            </v-list-group>
+
+            <v-list-group value="Actions">
+              <template v-slot:activator="{ props }">
+                <v-list-item v-bind="props" title="Actions"></v-list-item>
+              </template>
+
+              <v-list-item v-for="([title, icon], i) in cruds" :key="i" :value="title" :title="title"
+                :prepend-icon="icon"></v-list-item>
+            </v-list-group>
+          </v-list-group>
+        </v-list>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>
+            Logout
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+    </v-main>
+  </v-layout>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+
+export default {
+
+
+  setup() {
+    const open = ref('Users');
+    const admins = ref([['Management', 'mdi-account-multiple-outline'],
+    ['Settings', 'mdi-cog-outline']]);
+    const cruds = ref([
+      ['Create', 'mdi-plus-outline'],
+      ['Read', 'mdi-file-outline'],
+      ['Update', 'mdi-update'],
+      ['Delete', 'mdi-delete'],
+    ])
+
+    return { open, admins, cruds }
+  }
+}
+</script>
--- vue/vue.config.js
+++ vue/vue.config.js
@@ -3,13 +3,28 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir:"../src/main/resources/static",
   indexPath:"index.html",
+
   devServer:{
     port:9191,
     proxy:"http://localhost:9191",
   },
-  chainWebpack:config=>{
-    const svgRole = config.module.rule("svg");
-    svgRole.uses.clear();
-    svgRole.use("vue-svg-loader").loader("vue-svg-loader");
+
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
   }
 })
